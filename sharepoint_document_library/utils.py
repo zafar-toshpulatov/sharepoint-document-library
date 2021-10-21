7c52f8ac5efc30d6a3e660e5bbd4e22d750536ed
zafar-toshpulatov <ztoshpulatov@rfa.com>
2021-10-21 19:42:27 +0500

    updated config argument path to dictionary

--- sharepoint_document_library/utils.py
+++ sharepoint_document_library/utils.py
@@ -46,7 +46,7 @@ def parse_args(required_config_keys):
     args = parser.parse_args()
     if args.config:
         setattr(args, 'config_path', args.config)
-        args.config = json.loads(args.config)
+        args.config = json.loads((args.config).replace("\'", "\""))
         # args.config = load_json(args.config)
     if args.download_path:
         setattr(args, 'download_path', args.download_path)
